19dce4d8c1fde8c37e04cc093b2b0f06f18c0953
Choi Yoons <ys.choi@me.com>
2018-06-24 23:33:57 +0900

    Photo Panel Update

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,21 +1,14 @@
 <template>
   <div>
-    <h1>Album Component</h1>
     <div id="photo-container">
-      <photo-panel></photo-panel>
-      <photo-panel></photo-panel>
-      <photo-panel></photo-panel>
-      <photo-panel></photo-panel>
-      <photo-panel></photo-panel>
-      <photo-panel></photo-panel>
-      <photo-panel></photo-panel>
+      <photo-panel v-for="(photo, index) in albums" :key="index" :src="photo.hash"/>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-
+import { Getter, Action } from "vuex-class";
 import PhotoPanel from './PhotoPanel.vue';
 
 @Component({
@@ -24,19 +17,43 @@ import PhotoPanel from './PhotoPanel.vue';
   }
 })
 export default class albumComponent extends Vue {
-  
+  @Getter('getAlbum', { namespace: 'album' }) albums: any;
+  @Action('loadAlbum', { namespace: 'album' }) loadAlbum: any;
+
+  created() {
+  }
+
+  mounted() {
+    this.loadAlbum();
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 div {
   margin: auto;
-  width: 960px;
 
   div#photo-container {
     width: inherit;
-    display: flex;
-    flex-flow: row wrap;
+  }
+
+  @media only screen and (min-width: 1201px) {
+    div{
+      width: 1200px;
+
+      div#photo-container {
+        column-count: 5;
+      }
+    }
+  }
+
+  @media only screen and (max-width: 1200px) and (min-width: 1024px) {
+    div{
+      width: 960px;
+      div#photo-container {
+        column-count: 4;
+      }
+    }
   }
 }
 </style>
--- src/components/PhotoPanel.vue
+++ src/components/PhotoPanel.vue
@@ -1,36 +1,53 @@
 <template>
   <div class="photo-panel">
-    <img src="http://localhost:8080/ipfs/QmdxUtcsrcyFxzbLJyfijp8z1cRKbinuRfUgsDqeAFeL1h">
+    <img :src="fullUrl">
+    <section>
+      asdf
+    </section>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component({
   components: {
   }
 })
 export default class PhotoPanel extends Vue {
+
+  @Prop({ type: String, default: 'QmesQMPamSbqNqVi2QqVto5fNYdWFi27PnvtVEbh5hY9ua' }) src: string;
+
+  get fullUrl() {
+    console.log()
+    return `http://localhost:8080/ipfs/${this.src}`;
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 div.photo-panel {
   width: 228px;
-  max-width: 228px;
-  min-height: 100px;
-  max-height: 250px;
+  min-height: 150px;
+  max-height: 400px;
   margin-top: 5px;
   margin-right: 5px;
   margin-left: 5px;
   margin-bottom: 5px;
   border-radius: 0.25rem;
   border: 1px solid #ced4da;
+  display: inline-block;
 
   img {
+    object-fit: cover;
     width: 100%;
+    height: 100%;
     border-radius: 0.25rem;
+  }
+
+  section {
+    width: 100%;
+    height: 80px;
   }
 }
 </style>

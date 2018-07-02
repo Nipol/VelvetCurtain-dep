a47b8023e1742695ac7329651df984f8d8cda7b6
Choi Yoons <ys.choi@me.com>
2018-07-02 12:37:57 +0900

    Add Sidebar

--- src/app.vue
+++ src/app.vue
@@ -1,10 +1,9 @@
 <template>
   <div>
-    <Navbar></Navbar>
+    <Navbar/>
     <div id="container">
       <router-view></router-view>
-      <div id="side-bar-container">
-      </div>
+      <Sidebar/>
     </div>
   </div>
 </template>
@@ -13,10 +12,12 @@
 import { Component, Vue } from 'vue-property-decorator';
 
 import Navbar from './components/Navbar.vue';
+import Sidebar from './components/Sidebar.vue';
 
 @Component({
   components: {
-    'Navbar': Navbar
+    'Navbar': Navbar,
+    'Sidebar': Sidebar,
   }
 })
 export default class App extends Vue {
@@ -32,16 +33,5 @@ export default class App extends Vue {
 div#container {
   padding-top: 42px;
   height: calc(100vh - 42px);
-}
-
-div#side-bar-container {
-  width: 240px;
-  height: calc(100% - 40px);
-  position: fixed;
-  top: 40px;
-  background: #f8f9fc;
-
-  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
-  transition: all 0.3s cubic-bezier(.25,.8,.25,1);
 }
 </style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <div id="side-bar-container">
+      <div class="menubox">
+        <h1>
+          <font-awesome-icon icon="search" /> &nbsp; Browser
+        </h1>
+        <ul>
+          <router-link tag="li" :to="{ 'name': 'Photos' }">
+            Photos
+          </router-link>
+        </ul>
+      </div>
+
+      <div class="menubox">
+        <h1>
+          <font-awesome-icon icon="images" /> &nbsp; Album
+        </h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+@Component({
+  components: {
+    'font-awesome-icon': FontAwesomeIcon
+  }
+})
+export default class Sidebar extends Vue {
+  beforeCreate() {
+  }
+
+  created () {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+div#side-bar-container {
+  width: 240px;
+  height: calc(100% - 38px);
+  position: fixed;
+  top: 38px;
+  background: #f8f9fc;
+
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+
+  padding-left: 1rem;
+
+  div.menubox {
+    margin-bottom: 48px;
+
+    h1 {
+      font-weight: 500;
+      color: #6c757d;
+      font-size: 0.9rem;
+    }
+
+    ul {
+      list-style: none;
+      padding-left: 0;
+      padding-right: 1rem;
+
+      li {
+        font-weight: 400;
+        color: rgba(0,0,0,0.40);
+        line-height: 1.8rem;
+        background-color: inherit;
+        padding-left: 0.5rem;
+
+        &.active {
+          font-weight: 400;
+          border-radius: 0.25rem;
+          color: #f8f9fc;
+          background-color: #838a91;
+        }
+      }
+    }
+  }
+}
+</style>
